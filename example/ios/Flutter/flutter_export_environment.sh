99f240e18e8761c23d62f97dff1b4138b12e6656
Felipe Szulcsewski <thefelipexu@gmail.com>
2019-09-16 11:59:45 -0300

    updates dependencies

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/felipexu/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/felipexu/Desktop/Skoreio/flutter_plugin_pdf_viewer/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/felipexu/Documents/flutter/bin/cache/artifacts/engine/ios"
